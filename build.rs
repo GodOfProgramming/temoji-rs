6741db23bb96f1f26eab530a294a48cb962e261e
Jonathan Dickert <jonathan@rocketscience.gg>
2023-08-24 11:56:08 -0400

    binary format

---
+++ build.rs
@@ -0,0 +1,32 @@
+use std::{error::Error, fs};
+
+pub const BG_CHAR: &str = "\u{00FF}";
+pub const FG_CHAR: &str = "\u{00FE}";
+
+const BG_TMPL_CHAR: char = '-';
+const FG_TMPL_CHAR: char = '*';
+
+fn main() -> Result<(), Box<dyn Error>> {
+    for file in fs::read_dir("assets")? {
+        let file = file?;
+        if file.file_type()?.is_file() {
+            let data = fs::read_to_string(file.path())?;
+            let mut path = file.path();
+            let stem = path
+                .file_stem()
+                .ok_or("cannot get file stem somehow")?
+                .to_str()
+                .ok_or("cannot convert to str")?
+                .to_string();
+            path.pop();
+
+            fs::write(
+                path.join("bin").join(format!("{}.bin", stem)),
+                data.replace(BG_TMPL_CHAR, BG_CHAR)
+                    .replace(FG_TMPL_CHAR, FG_CHAR),
+            )?;
+        }
+    }
+
+    Ok(())
+}
---
+++ src/main.rs
@@ -0,0 +1,55 @@
+use clap::Parser;
+use rust_embed::RustEmbed;
+use std::error::Error;
+
+pub const BG_CHAR: &str = "\u{00FF}";
+pub const FG_CHAR: &str = "\u{00FE}";
+
+const HEIGHT: usize = 7;
+
+#[derive(RustEmbed)]
+#[folder = "assets/bin"]
+struct Assets;
+
+#[derive(Parser, Debug)]
+struct Args {
+    #[arg(short, long)]
+    text: String,
+    #[arg(short, long)]
+    foreground: String,
+    #[arg(short, long)]
+    background: String,
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args = Args::parse();
+
+    let fg = args.foreground.to_lowercase();
+    let bg = args.background.to_lowercase();
+
+    let mut letters = Vec::with_capacity(args.text.len());
+
+    for c in args.text.chars() {
+        let c_bytes = Assets::get(&format!("{}.bin", c))
+            .or_else(|| Assets::get(&format!("0x{:x}.bin", c as u32)))
+            .ok_or_else(|| format!("could not load file assets/{}.bin (0x{:x})", c, c as u32))?;
+
+        let c_str = unsafe { String::from_utf8_unchecked(c_bytes.data.into()) };
+
+        letters.push(
+            c_str
+                .lines()
+                .map(|l| l.to_string().replace(BG_CHAR, &bg).replace(FG_CHAR, &fg))
+                .collect::<Vec<String>>(),
+        );
+    }
+
+    for i in 0..HEIGHT {
+        for letter in letters.iter() {
+            print!("{}", letter[i]);
+        }
+        println!();
+    }
+
+    Ok(())
+}
