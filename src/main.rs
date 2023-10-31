a85923c9c75dde1c248792411957eace9316573a
Jonathan Dickert <jonathan.dickert@protonmail.com>
2023-10-31 10:13:53 -0400

    did stuff

--- src/main.rs
+++ src/main.rs
@@ -24,12 +24,11 @@ struct Args {
 fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
 
-    let fg = args.foreground.to_lowercase();
-    let bg = args.background.to_lowercase();
+    let text = args.text.to_lowercase();
 
     let mut letters = Vec::with_capacity(args.text.len());
 
-    for c in args.text.chars() {
+    for c in text.chars() {
         let c_bytes = Assets::get(&format!("{}.bin", c))
             .or_else(|| Assets::get(&format!("0x{:x}.bin", c as u32)))
             .ok_or_else(|| format!("could not load file assets/{}.bin (0x{:x})", c, c as u32))?;
@@ -39,7 +38,11 @@ fn main() -> Result<(), Box<dyn Error>> {
         letters.push(
             c_str
                 .lines()
-                .map(|l| l.to_string().replace(BG_CHAR, &bg).replace(FG_CHAR, &fg))
+                .map(|l| {
+                    l.to_string()
+                        .replace(BG_CHAR, &args.background)
+                        .replace(FG_CHAR, &args.foreground)
+                })
                 .collect::<Vec<String>>(),
         );
     }
