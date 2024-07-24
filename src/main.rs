4e500d9b1036a48782f44dfaa0aa8b5839f9d7f0
Jonathan Dickert <jonathan.dickert@protonmail.com>
2024-07-24 10:02:38 -0400

    added multiline support

--- src/main.rs
+++ src/main.rs
@@ -13,46 +13,56 @@ struct Assets;
 
 #[derive(Parser, Debug)]
 struct Args {
-    #[arg(short, long)]
-    text: String,
-    #[arg(short, long)]
-    foreground: String,
-    #[arg(short, long)]
-    background: String,
+  #[arg(short, long)]
+  text: String,
+  #[arg(short, long)]
+  foreground: String,
+  #[arg(short, long)]
+  background: String,
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let args = Args::parse();
+  let args = Args::parse();
 
-    let text = args.text.to_lowercase();
+  let text = args.text.to_lowercase();
 
-    let mut letters = Vec::with_capacity(args.text.len());
+  let mut lines = Vec::with_capacity(args.text.lines().count());
+  let mut letters = Vec::with_capacity(args.text.len());
 
-    for c in text.chars() {
-        let c_bytes = Assets::get(&format!("{}.bin", c))
-            .or_else(|| Assets::get(&format!("0x{:x}.bin", c as u32)))
-            .ok_or_else(|| format!("could not load file assets/{}.bin (0x{:x})", c, c as u32))?;
+  for c in text.chars() {
+    if c == '\n' {
+      lines.push(letters.clone());
+      letters.clear();
+    } else {
+      let c_bytes = Assets::get(&format!("{}.bin", c))
+        .or_else(|| Assets::get(&format!("0x{:x}.bin", c as u32)))
+        .ok_or_else(|| format!("could not load file assets/{}.bin (0x{:x})", c, c as u32))?;
 
-        let c_str = unsafe { String::from_utf8_unchecked(c_bytes.data.into()) };
+      let c_str = unsafe { String::from_utf8_unchecked(c_bytes.data.into()) };
 
-        letters.push(
-            c_str
-                .lines()
-                .map(|l| {
-                    l.to_string()
-                        .replace(BG_CHAR, &args.background)
-                        .replace(FG_CHAR, &args.foreground)
-                })
-                .collect::<Vec<String>>(),
-        );
+      letters.push(
+        c_str
+          .lines()
+          .map(|l| {
+            l.to_string()
+              .replace(BG_CHAR, &args.background)
+              .replace(FG_CHAR, &args.foreground)
+          })
+          .collect::<Vec<String>>(),
+      );
     }
+  }
 
+  lines.push(letters);
+
+  for letters in lines {
     for i in 0..HEIGHT {
-        for letter in letters.iter() {
-            print!("{}", letter[i]);
-        }
-        println!();
+      for letter in letters.iter() {
+        print!("{}", letter[i]);
+      }
+      println!();
     }
+  }
 
-    Ok(())
+  Ok(())
 }
